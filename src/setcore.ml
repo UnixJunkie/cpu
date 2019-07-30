8d418b0db383ca158639f691eaf84ad8381ca02c
Francois Berenger <unixjunkie@sdf.org>
2019-07-30 16:39:22 +0900

    a little bit of ocamldoc doesn't hurt

--- src/setcore.ml
+++ src/setcore.ml
@@ -2,5 +2,8 @@
    declare that the current process should be
    attached to core number n *)
 
+(** [numcores ()] get the number of processors online. *)
 external numcores: unit -> int = "numcores"
+
+(** [setcore i] pin current process to core number [i]. *)
 external setcore: int -> unit = "setcore"
