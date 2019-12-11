bed204cbffc06cb3f69bc49746df8de13d589dbb
Francois Berenger <unixjunkie@sdf.org>
2019-12-11 11:57:03 +0900

    rm src/setcore.ml

--- src/setcore.ml
+++
@@ -1,9 +0,0 @@
-(* uses the native affinity interface to
-   declare that the current process should be
-   attached to core number n *)
-
-(** [numcores ()] get the number of CPU cores online. *)
-external numcores: unit -> int = "numcores"
-
-(** [setcore i] pin current process to core number [i]. *)
-external setcore: int -> unit = "setcore"
